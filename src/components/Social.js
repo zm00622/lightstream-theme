1bbbf9ce7cc911428f9d043bea4df7fe0a66c88c
zm00622 <zarymanning@gmail.com>
2020-02-19 14:41:54 -0500

    Removing Broken Link

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,7 +7,6 @@ class Social extends Component {
             <a href="https://codepen.io/RiseOverLimits" target="_blank"><i class="fab fa-codepen"></i></a>
             <a href="https://github.com/zm00622" target="_blank"><i class="fab fa-github"></i></a>
             <a href="https://www.instagram.com/zarymanning/" target="_blank"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/profile.php?id=100032101175088" target="_blank"><i class="fab fa-facebook-f"></i></a>
             <a href="https://www.linkedin.com/in/zary-manning-4b8953198/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
             <a href="https://medium.com/@zarymanning" target="_blank"><i class="fab fa-medium-m"></i></a>
             </div>
